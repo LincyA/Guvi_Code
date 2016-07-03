684e84d6fb4af91df00b7cfcf892701d8f1795e8
LincyA <lincy2302@gmail.com>
2016-07-03 21:05:06 +0530

    Create GreatestNumber.java

---
+++ GreatestNumber.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+class GreatestNumber
+{
+public static void main(String[] a)
+{ 
+System.out.println("Enter 3 numbers = ");
+Scanner sc = new Scanner(System.in);
+int num1 = sc.nextInt();
+int num2 = sc.nextInt();
+int num3 = sc.nextInt();
+if((num1>num2)&&(num1>num3))
+{
+System.out.println(num1+"is the greatest");
+}
+else if((num2>num3))
+{
+System.out.println(num2+"is the greatest");
+}
+else
+{
+System.out.println(num3+"is the greatest");
+}
+}}
