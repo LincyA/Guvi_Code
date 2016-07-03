be6fcd9be25bc2a6052d2b5e94552230edd4bfca
LincyA <lincy2302@gmail.com>
2016-07-03 22:30:00 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class Sum
+{
+public static void main(String[] a)
+{ 
+int sum=0;
+System.out.println("Enter a number = ");
+Scanner sc = new Scanner(System.in);
+int number = sc.nextInt();
+for(int i=1;i<=number;i++)
+    {
+        sum += i; 
+    }
+System.out.println("Sum of given"+number+"natural numbers = "+sum);
+}}
