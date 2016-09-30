474d76b1a20b8146566ea71a419a3f02d546fb9a
LincyA <lincy2302@gmail.com>
2016-09-30 14:15:49 +0530

    to print # in steps

--- hashStep.java
+++ hashStep.java
@@ -3,9 +3,9 @@ class hashStep
 {
 public static void main(String args[])
 {
-	for(int i=1;i<=10;i++)
-   {
- for(int j=1;j<=i;j++)
+for(int i=1;i<=10;i++)
+{
+for(int j=1;j<=i;j++)
 {
 System.out.print("#"+" ");
 }
